357c352831f7c33389eb867a09b46242cc1d4e70
PzOng <pangszee.ong@gmail.com>
2020-07-21 00:00:21 +0930

    Updates

---
+++ precache-manifest.914f163be5faabe4d8115f1f607a066c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91f85bda48773c1389d6c75d96fb1d1",
+    "url": "/RACTP/index.html"
+  },
+  {
+    "revision": "af2b888240d6d9038bfc",
+    "url": "/RACTP/static/css/main.a25c7dda.chunk.css"
+  },
+  {
+    "revision": "f765166374dca4cc9b8d",
+    "url": "/RACTP/static/js/2.18156b24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RACTP/static/js/2.18156b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2b888240d6d9038bfc",
+    "url": "/RACTP/static/js/main.6c5915e2.chunk.js"
+  },
+  {
+    "revision": "e7c7ac8ca5d03141188e",
+    "url": "/RACTP/static/js/runtime-main.75dccb9d.js"
+  },
+  {
+    "revision": "063765a1f07c7ed99a095cec3a22732a",
+    "url": "/RACTP/static/media/cheers.063765a1.png"
+  },
+  {
+    "revision": "cc87294e92feff6043642637f0429eb0",
+    "url": "/RACTP/static/media/home1.cc87294e.png"
+  },
+  {
+    "revision": "33fe28508b5d5b33667a4a3b98e8b1b5",
+    "url": "/RACTP/static/media/home2.33fe2850.png"
+  },
+  {
+    "revision": "f9cda80c22638ab0726c084c63a2d00d",
+    "url": "/RACTP/static/media/wapp.f9cda80c.png"
+  }
+]);
