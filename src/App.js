73c181711bff46958a61db321026da92498799c7
PzOng <pangszee.ong@gmail.com>
2020-07-22 20:16:27 +0930

    routing fix

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={Home} />
+          <Route exact path="/RACTP" component={Home} />
           <Route exact path="/home" component={Home} />  
           
           <Route exact path="/project" component={Project} />  
